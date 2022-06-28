fdec40bfdbe168811ecdd241ebfb357ab6f6efe7
Szymon Maciejewski <s20567@pjwstk.edu.pl>
2022-06-28 19:41:09 +0200

    fix parameter

--- kolokwium/Services/IOrganizationService.cs
+++ kolokwium/Services/IOrganizationService.cs
@@ -5,5 +5,5 @@ namespace kolokwium.Services;
 public interface IOrganizationService
 {
     public Task<TeamDTO> GetTeam(int id);
-    public Task AddMember(int id);
+    public Task AddMember(int idMember, int idTeam);
 }
